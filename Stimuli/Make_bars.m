f6182078142c9e97635d7922b5b85a9f454238fd
N-HEDGER <nhedger1@gmail.com>
2017-04-28 16:52:04 +0100

    Null trials and drawing mode

--- Stimuli/Make_bars.m
+++ Stimuli/Make_bars.m
@@ -3,9 +3,14 @@
 bars=cell(1,const.Nbars);
 h=waitbar(0,'Creating Stimuli...');
     for bar=1:const.Nbars
-        perc=(bar/const.Nbars)*100;
+        perc=floor((bar/const.Nbars)*100);
         waitbar(perc/100,h,sprintf('Drawing Stimuli... %d%%completed',perc));
         bars{bar}=colnoisebar(const.elementsize,const.barlength,const.barwidth,scr.vd,scr.scr_sizeX,scr.dispsize,0);
+        if getstimuli
+            bars{bar}(:,:,:)=1;
+        else
+        end
+           
     end
     close(h)
     
